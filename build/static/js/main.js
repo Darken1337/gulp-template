b73d4d2a4654a6c832031d56bd0c8d106f4c4fab
Darken1337 <40112493+Darken1337@users.noreply.github.com>
2020-03-24 14:12:22 +0300

    32

--- build/static/js/main.js
+++ build/static/js/main.js
@@ -1,67 +1,3 @@
-<<<<<<< HEAD
-svg4everybody();
-
-var menuDropInit = {
-    openMenu: function(menu){
-
-        var drops = $('[data-dropdown="body"]');
-
-        drops.hide(0);
-
-        menu.fadeIn(0);
-
-    },
-    init: function(){
-
-        var initiators = $('[data-dropdown="open"]'),
-            openMain = $('[data-dropdown="open-main"]'),
-            bodyMain = $('[data-dropdown="body-main"]')
-            overlay = $('.overlay'),
-            _this = this;
-
-        initiators.on('mouseenter', function(){
-
-            var dropToOpen = $(this).children('[data-dropdown="body"]');
-
-            _this.openMenu(dropToOpen);
-
-            initiators.removeClass('active');
-
-            $(this).addClass('active');
-
-        })
-
-        openMain.on('mouseenter', function(){
-
-            var dropToOpen = $(this).children('[data-dropdown="body-main"]');
-
-            _this.openMenu(dropToOpen);
-
-            $(this)
-            .css('z-index', parseInt(overlay.css('z-index')) + 1)
-            .addClass('active');
-
-            overlay.fadeIn(0);
-
-        })
-
-        
-        overlay.on('mouseenter', function(){
-
-            bodyMain.hide(0);
-            $(this).hide(0);
-
-            openMain.css('z-index', 1)
-                    .removeClass('active');
-            
-
-        })
-    }
-}
-
-
-menuDropInit.init();
-=======
 jQuery(document).ready(function($){
 
     //Menu init
@@ -156,4 +92,3 @@ jQuery(document).ready(function($){
     })
 
 })
->>>>>>> parent of d292c08... clean
